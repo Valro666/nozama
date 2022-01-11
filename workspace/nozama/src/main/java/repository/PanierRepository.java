3af737e3fd89a8a56e3d54e57771be6c796466e0
Valou <valerian266@gmail.com>
2022-01-11 21:43:40 +0100

    nettoyage pour spring boot

--- workspace/nozama/src/main/java/repository/PanierRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.CompteClient;
-
-public interface PanierRepository extends JpaRepository<CompteClient, String> {
-
-}
