b2c98919af280a1944cb9049d2f45961691f642d
Hvmine <hadrien.vignard@hotmail.fr>
2022-01-12 00:24:16 +0100

    Frontv0.1.1
    Multi-dropdown nav

--- nozama/src/app/app.module.ts
+++ nozama/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './webcomponents/nav/nav.component';
 import { Nav2Component } from './webcomponents/nav2/nav2.component';
+import { TestComponent } from './webcomponents/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    Nav2Component
+    Nav2Component,
+    TestComponent
   ],
   imports: [
     BrowserModule,
--- nozama/src/app/webcomponents/nav2/nav2.component.ts
+++ nozama/src/app/webcomponents/nav2/nav2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav2',
   templateUrl: './nav2.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Nav2Component implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
+
+
