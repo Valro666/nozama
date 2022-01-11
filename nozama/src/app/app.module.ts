2c3dcc553d837fc463e5255ba9c2238880197f58
Hvmine <hadrien.vignard@hotmail.fr>
2022-01-11 20:54:36 +0100

    Frontv0.1
    nav / boostrap

--- nozama/src/app/app.module.ts
+++ nozama/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './webcomponents/nav/nav.component';
+import { Nav2Component } from './webcomponents/nav2/nav2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    Nav2Component
   ],
   imports: [
     BrowserModule,
