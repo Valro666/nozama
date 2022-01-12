e5a2e45533333b2914f5225b96e72e0493bf978e
Hvmine <hadrien.vignard@hotmail.fr>
2022-01-12 12:25:42 +0100

    Frontv0.2
    Article + footer

--- nozama/src/app/app-routing.module.ts
+++ nozama/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleComponent } from './webcomponents/article/article.component';
 
 const routes: Routes = [
-  
+  { path: 'article/:id', component:ArticleComponent },
 ];
 
 @NgModule({
--- nozama/src/app/app.module.ts
+++ nozama/src/app/app.module.ts
@@ -5,14 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './webcomponents/nav/nav.component';
 import { Nav2Component } from './webcomponents/nav2/nav2.component';
-import { TestComponent } from './webcomponents/test/test.component';
+import { FooterComponent } from './webcomponents/footer/footer.component';
+import { ArticleComponent } from './webcomponents/article/article.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     Nav2Component,
-    TestComponent
+    FooterComponent,
+    ArticleComponent,
+
+
+
   ],
   imports: [
     BrowserModule,
