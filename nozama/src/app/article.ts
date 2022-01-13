8241e325048ed3081ab1a1d8274952c534c0372b
Robin B <robin.belvisi@gmail.com>
2022-01-13 12:41:54 +0100

    accueil page

---
+++ nozama/src/app/article.ts
@@ -0,0 +1,11 @@
+export class Article {
+    id:number;
+    nom:String;
+    prix:number;
+    img:String;
+    description:String;
+    categorie:String;
+    sousCategorie:String;
+    fabricant:String;
+    version:number
+}
--- nozama/src/app/webcomponents/acceuil/acceuil.component.ts
+++ nozama/src/app/webcomponents/acceuil/acceuil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { InfoService } from 'src/app/service/info.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { Article } from 'src/app/article';
 
 @Component({
   selector: 'app-acceuil',
@@ -7,10 +9,53 @@ import { InfoService } from 'src/app/service/info.service';
   styleUrls: ['./acceuil.component.css']
 })
 export class AcceuilComponent implements OnInit {
-
-  constructor(private info : InfoService) { }
+nbAritcles:number
+article:Article
+tabArticles:any
+  
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
-  }
+    this.http.get<Article>("http://localhost:8080/tp/api/article").subscribe(
+      response => {
+        this.tabArticles= response;
+        
+      },
+      err => {
+        console.log("**********KO")
+      }
+    );
+  };
+
+  buildArr(theArr: Article[]): Article[][]{
+
+    var arrOfarr = [];
+    for(var i = 0; i < theArr.length ; i+=5) {
+        var row = [];
+
+        for(var x = 0; x < 5; x++) {
+          var value = theArr[i + x];
+            if (!value) {
+                break;
+            }
+            row.push(value);
+        }
+        arrOfarr.push(row);
+    }
+     return arrOfarr;
+}
+    cutOutArticles(){
+      
+      for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < 4; j++) {
+          this.tabArticles[i][j]=this.article[i];
+        }
+      }
+     
+    }
+  
+ 
 
 }
+
+
