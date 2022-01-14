defb1b26cc9bd28c80780ab9ae07bb8972d5f264
Hvmine <hadrien.vignard@hotmail.fr>
2022-01-14 15:42:53 +0100

    panier ne marche pas

--- nozama/src/app/webcomponents/panier/panier.component.ts
+++ nozama/src/app/webcomponents/panier/panier.component.ts
@@ -1,7 +1,10 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Element } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 
 import { Article } from 'src/app/class/article';
+import { InfoService } from 'src/app/service/info.service';
 
 @Component({
   selector: 'app-panier',
@@ -14,26 +17,53 @@ export class PanierComponent implements OnInit {
   prixtt = 0
   span : any[]
   login:any
+  spa:any[]
  
-  constructor() { }
+  constructor(private http:HttpClient, private info : InfoService) { }
 
 
   ngOnInit(): void {
     this.panier = JSON.parse(localStorage.getItem('panier'));
-    this.login = JSON.parse(localStorage.getItem("infoclient")).nom;
+    this.login = JSON.parse(localStorage.getItem("infoclient")).login;
+    this.spa = []
     
   }
 
   sendpanier(){
-    var sp
-    var ligne
+    
     this.panier.forEach(element => {
-      ligne = ligne + [element , element.quantite]
+      let semiligne = [element , element.quantite]
+      this.spa.push(semiligne);
     });
 
-    var date = "2022-01-13T23:00:00.000+00:00"
-     this.span = [this.panier , date ,this.login ];
+   
+    let date = "2022-01-13T23:00:00.000+00:00"
+     this.span = [ this.spa,this.login ];
+    let body = JSON.stringify(this.span);
 
+    this.info.debugerr(body)
+
+     this.http.post("http://localhost:8080/tp/panier", body,{
+      headers : new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+
+      
+
+    }).
+    subscribe(response => {
+
+      console.log("crud service post OK");
+      
+
+    },
+
+      err => {
+        console.log("crud service post KO")
+        
+      });
+
+      
   }
 
   deletearticle(x) {
